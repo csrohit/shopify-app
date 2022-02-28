9c5c310c4ca43237c2d08788ed632b352fd04a0d
Rohit Nimkar <nehalnimkar@gmail.com>
2022-02-28 07:17:03 +0530

    :tada: Initial commit

---
+++ src/test/java/io/csrohit/shopifyapp/ShopifyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.csrohit.shopifyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopifyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
